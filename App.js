d90ab0e574880901e41f4324977b7c3f27a88df1
Giuseppe Antonio Setem Davanzo <giuseppe.davanzo@ws-verita.sede>
2019-09-26 11:13:07 -0300

    Scrollable Screen

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import {
   StyleSheet,
-  Text,
   View,
   FlatList,
   ScrollView,
-  Dimensions
+  Dimensions,
+  Platform,
+  Animated
 } from "react-native";
 import {
   Appbar,
@@ -44,19 +45,24 @@ function App() {
 }
 
 class Content extends Component {
+  // Animation
+  _scrollValue = new Animated.Value(0);
+
   state = {
     data: ["Project", "About", "Reviews", "Community", "Files"],
     dataIndex: 0
   };
 
+  _flatlistRef = React.createRef();
+
   _viewabilityConfig = {
-    itemVisiblePercentThreshold: 50
+    itemVisiblePercentThreshold: 45
   };
 
+  _IOS = Platform.OS === "ios";
+
   _onViewableItemsChanged = info => {
-    const { index = 0 } = info.viewableItems[0]
-      ? info.viewableItems[0]
-      : { index: 0 };
+    const { index = 0 } = info.viewableItems[0];
 
     this.setState({ dataIndex: index });
   };
@@ -67,6 +73,44 @@ class Content extends Component {
     index
   });
 
+  _onScrollEndDrag = event => {
+    const { dataIndex, data } = this.state;
+    const speed = event.nativeEvent.velocity.x;
+    let index = dataIndex;
+
+    if (this._IOS) {
+      if (speed > 1 && index < data.length - 1) {
+        index += 1;
+      }
+      if (speed < -1 && index > 0) {
+        index -= 1;
+      }
+    } else {
+      if (speed < -1 && dataIndex < data.length - 1) {
+        index += 1;
+      }
+      if (speed > 1 && dataIndex > 0) {
+        index -= 1;
+      }
+    }
+
+    this._flatlistRef.current.scrollToIndex({
+      index: index,
+      animated: true,
+      viewPosition: 0.5
+    });
+    this.setState({ dataIndex: index });
+  };
+
+  _navOnClick = index => {
+    this._flatlistRef.current.scrollToIndex({
+      index: index,
+      animated: true,
+      viewPosition: 0.5
+    });
+    this.setState({ dataIndex: index });
+  };
+
   render() {
     const { data, dataIndex } = this.state;
 
@@ -76,34 +120,52 @@ class Content extends Component {
         <FlatList
           style={styles.flatListMenu}
           data={data}
+          extraData={dataIndex}
           keyExtractor={(item, index) => index.toString()}
           horizontal
           showsHorizontalScrollIndicator={false}
           renderItem={({ item, index }) => (
+            // <Animated.View
+            //   style={[
+            //     {
+            //       backgroundColor: this._scrollValue.interpolate({
+            //         inputRange: [0, screenWidth * index + dataIndex],
+            //         outputRange: ["#CCC", DefaultTheme.colors.primary],
+            //         extrapolate: "clamp"
+            //       }),
+            //       marginLeft: 11
+            //     },
+            //     styles.indicator
+            //   ]}
+            // />
             <ListButton
               style={styles.flatListItem}
-              isSelected={index == dataIndex}
+              isSelected={index === dataIndex}
               title={item}
-              onPress={() => {}}
+              onPress={() => this._navOnClick(index)}
             />
           )}
         />
         {/* Content */}
         <FlatList
+          onScroll={Animated.event([
+            {
+              nativeEvent: { contentOffset: { x: this._scrollValue } }
+            }
+          ])}
+          ref={this._flatlistRef}
           style={styles.flatListContent}
           data={data}
           keyExtractor={(item, index) => index.toString()}
           horizontal
           showsHorizontalScrollIndicator={false}
           getItemLayout={this._itemLayout}
-          viewabilityConfig={this._viewabilityConfig}
           onViewableItemsChanged={this._onViewableItemsChanged}
-          // decelerationRate="fast"
-          // bounces={true}
-          // snapToInterval={screenWidth}
-          // onViewableItemsChanged={this._onViewableItemsChanged}
-          // viewabilityConfig={this._viewabilityConfig}
-
+          viewabilityConfig={this._viewabilityConfig}
+          onScrollEndDrag={this._onScrollEndDrag}
+          decelerationRate="fast"
+          bounces={true}
+          snapToInterval={screenWidth}
           renderItem={({ item, index }) => (
             <ScrollView style={styles.contentContainer}>
               <Paragraph>{item}</Paragraph>
@@ -139,13 +201,13 @@ const styles = StyleSheet.create({
     backgroundColor: theme.colors.primary
   },
   flatListContainer: {
-    paddingHorizontal: 14,
+    // paddingHorizontal: 14,
     borderTopRightRadius: 14,
     borderTopLeftRadius: 14,
     backgroundColor: theme.colors.background
   },
   contentContainer: {
-    width: screenWidth - 28
+    width: screenWidth
   },
   flatListContent: {
     backgroundColor: theme.colors.background,
@@ -153,10 +215,16 @@ const styles = StyleSheet.create({
   },
   flatListMenu: {
     marginVertical: 24,
+    marginHorizontal: 14,
     height: 56
   },
   flatListItem: {
     marginHorizontal: 14
+  },
+  indicator: {
+    height: 7,
+    width: 7,
+    borderRadius: 2
   }
 });
 
